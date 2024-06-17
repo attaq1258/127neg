e257b445ffacc511a48ac1da99e746768c65d998
attaq1258 <93429321+attaq1258@users.noreply.github.com>
2024-06-17 17:14:54 +0200

    Add files via upload

--- over.js
+++ over.js
@@ -1,4 +1,13 @@
 var x=0;var onscr=false;
+function wordWrap(text) {
+const regex=/[бвгджзйклмнпрстфхцчшщ]+[аеёиоуыэюя][бвгджзйклмнпрстфхцчшщ](?=[бвгджзйклмнпрстфхцчшщьъ ])[ьъй]?|[бвгджзйклмнпрстфхцчшщ]+[аеёиоуыэюя][й]?|[аеёиоуыэюя][бвгджзйклмнпрстфхцчшщ](?=[бвгджзйклмнпрстфхцчшщьъ ])[ъь]?|[аеёиоуыэюя](?=[а-я]{2})|(?<= +)[^\s]+(?= +|$)/gmi;
+let out='', offset=0, prev=0, pass=false;
+let matches=text.matchAll(regex);
+for (const m of matches) {
+let hyphen=text[prev - 1]==' '?'':'\n';
+out += text.substring(offset, prev)+hyphen;offset=prev;
+if (!pass)prev=m.index; pass=m[0].length < 2;}
+out += text.substring(offset);return out;}
 
 function obox(){if(input[x]){setTimeout(obox1,3000);
 document.getElementById('slde').style.visibility='hidden';
@@ -7,9 +16,10 @@ document.getElementById('slde').style.MozTransform='none';
 document.getElementById('slde').style.msTransform='none';
 document.getElementById('slde').style.OTransform='none';
 document.getElementById('slde').style.transform='none';
-document.getElementById('text').innerHTML=input[x];x+=1;
+var txtinp=input[x];txtinp=wordWrap(txtinp);
+document.getElementById('text').innerHTML=txtinp;x+=1;
 document.getElementById('metr').innerHTML=Array(x).join('_')+'&#9973\;'+Array(input.length-x+1).join('_')+'&#9875\;';}
-else{window.location.href="http://arbaat.awardspace.us/";}}
+else{window.location.href="http://neguru.us.to/";}}
 
 function obox1(){var ey=document.body.scrollTop+500;
 document.getElementById('slde').style.visibility='visible';
